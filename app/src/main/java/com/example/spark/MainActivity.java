b54c2da638a18db2ffca41610f3e632e37b711bb
Pinsara weerasinghe <pinsarakweerasinghe@gmail.com>
2019-04-05 18:16:04 +0530

    Image upload section settled

--- app/src/main/java/com/example/spark/MainActivity.java
+++ app/src/main/java/com/example/spark/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.spark;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -18,7 +27,7 @@ import android.widget.Toast;
 
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,SubmitReport.OnFragmentInteractionListener,History.OnFragmentInteractionListener,Help.OnFragmentInteractionListener, Register.OnFragmentInteractionListener, Login.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SubmitReport.OnFragmentInteractionListener, History.OnFragmentInteractionListener, Help.OnFragmentInteractionListener, Register.OnFragmentInteractionListener, Login.OnFragmentInteractionListener {
 
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
@@ -28,48 +37,107 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     static String userID;
     static String emailID;
 
+//    private LocationManager locationManager;
+//    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        session=new UserSessionManager(getApplicationContext());
+        session = new UserSessionManager(getApplicationContext());
 
         //btnLogout=(Button)findViewById(R.id.)
-        if(session.checkLogin()){
+        if (session.checkLogin()) {
             finish();
         }
 
 
+        HashMap<String, String> user = session.getUserDetails();
+        String uname = user.get(UserSessionManager.KEY_NAME);
+        String uemail = user.get(UserSessionManager.KEY_EMAIL);
 
-        HashMap<String,String> user=session.getUserDetails();
-        String uname=user.get(UserSessionManager.KEY_NAME);
-        String uemail=user.get(UserSessionManager.KEY_EMAIL);
+        userID = uname;
+        emailID = uemail;
 
-        userID=uname;
-        emailID=uemail;
-
-        Toolbar toolbar =findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        drawerLayout=findViewById(R.id.drawer_layout);
-        navigationView=findViewById(R.id.nav_view);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
 
+//        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+//        locationListener = new LocationListener() {
+//            @Override
+//            public void onLocationChanged(Location location) {
+//                Log.d("sss", String.valueOf(location.getLatitude()));
+//                Log.d("sss", String.valueOf(location.getLongitude()));
+//            }
+//
+//            @Override
+//            public void onStatusChanged(String provider, int status, Bundle extras) {
+//
+//            }
+//
+//            @Override
+//            public void onProviderEnabled(String provider) {
+//
+//            }
+//
+//            @Override
+//            public void onProviderDisabled(String provider) {
+//                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+//                startActivity(intent);
+//            }
+//        };
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                requestPermissions(new String[]{
+//                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET
+//                }, 101);
+//            }
+//            return;
+//        } else {
+//            configureButton();
+//        }
 
-        if (savedInstanceState==null){
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new SubmitReport()).commit();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SubmitReport()).commit();
         }
 
 
     }
 
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        switch (requestCode) {
+//            case 101:
+//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+//                    configureButton();
+//                return;
+//        }
+//    }
+//
+//    private void configureButton() {
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        locationManager.requestLocationUpdates("gps", 2000, 0, locationListener);
+//    }
 
 
     @Override
--- app/src/main/java/com/example/spark/SubmitReport.java
+++ app/src/main/java/com/example/spark/SubmitReport.java
@@ -1,16 +1,23 @@
 package com.example.spark;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.util.Base64;
 import android.util.Log;
@@ -25,21 +32,29 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+
+import net.gotev.uploadservice.MultipartUploadRequest;
+import net.gotev.uploadservice.UploadNotificationConfig;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.UUID;
 
 
 /**
@@ -56,12 +71,12 @@ public class SubmitReport extends Fragment implements AdapterView.OnItemSelected
     MapView mapView;
 
 
-    String locationRe="Testing location";
+    String locationRe = "Testing location";
     View view;
     String methodOfReport;
     CardView submit;
     EditText commit;
-    String userID=MainActivity.userID;
+    String userID = MainActivity.userID;
     String images;
     String lat;
     String lng;
@@ -76,10 +91,12 @@ public class SubmitReport extends Fragment implements AdapterView.OnItemSelected
     private LinearLayout image;
     private ImageView imageView;
     private TextView textView;
+    private static Uri uri;
+    private static String imageName;
+    private static int STORAGE_PERMISSION_CODE = 3;
 
     CardView button;
-    int PLACE_PICKER_REQUEST=1;
-
+    int PLACE_PICKER_REQUEST = 1;
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -128,18 +145,20 @@ public class SubmitReport extends Fragment implements AdapterView.OnItemSelected
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view=inflater.inflate(R.layout.fragment_submit_report, container, false);
+        view = inflater.inflate(R.layout.fragment_submit_report, container, false);
 
-        Spinner spinner=view.findViewById(R.id.incidentMe);
-        ArrayAdapter<CharSequence> charSequenceArrayAdapter=ArrayAdapter.createFromResource(getContext(),R.array.reportType,android.R.layout.simple_spinner_item);
+
+        Spinner spinner = view.findViewById(R.id.incidentMe);
+        ArrayAdapter<CharSequence> charSequenceArrayAdapter = ArrayAdapter.createFromResource(getContext(), R.array.reportType, android.R.layout.simple_spinner_item);
         charSequenceArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(charSequenceArrayAdapter);
         spinner.setOnItemSelectedListener(this);
 
+        requestLocationPermission();
 
 //        button=(CardView) view.findViewById(R.id.location);
-        submit=(CardView) view.findViewById(R.id.submitR);
-        commit=(EditText) view.findViewById(R.id.cmnt);
+        submit = (CardView) view.findViewById(R.id.submitR);
+        commit = (EditText) view.findViewById(R.id.cmnt);
         image = (LinearLayout) view.findViewById(R.id.scrollpad);
         imageView = (ImageView) view.findViewById(R.id.image_view);
         textView = (TextView) view.findViewById(R.id.text_view);
@@ -160,7 +179,7 @@ public class SubmitReport extends Fragment implements AdapterView.OnItemSelected
 //            }
 //        });
 
-        buttonc=(CardView) view.findViewById(R.id.buttonc);
+        buttonc = (CardView) view.findViewById(R.id.buttonc);
 
         buttonc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -168,7 +187,7 @@ public class SubmitReport extends Fragment implements AdapterView.OnItemSelected
                 Intent intent = new Intent();
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"),GET_FROM_GALLERY);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), GET_FROM_GALLERY);
             }
         });
 
@@ -176,15 +195,17 @@ public class SubmitReport extends Fragment implements AdapterView.OnItemSelected
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                LatLng center = googleMap.getCameraPosition().target;
-                lat= String.valueOf(center.latitude);
-                lng= String.valueOf(center.longitude);
-                submitReport();
+                if (requestStoragePermission()) {
+                    LatLng center = googleMap.getCameraPosition().target;
+                    lat = String.valueOf(center.latitude);
+                    lng = String.valueOf(center.longitude);
+//                    uploadMultipart(uri, "http://10.0.2.2:8080/spark/mobileapp/upload.php");
+                    submitReport();
+                }
             }
         });
 
-        return  view;
+        return view;
     }
 
     private void getFileUri() {
@@ -197,32 +218,30 @@ public class SubmitReport extends Fragment implements AdapterView.OnItemSelected
     }
 
 
-
-
     private void submitReport() {
-        String location=locationRe;
-        String methodof=methodOfReport;
-        String comment=commit.getText().toString();
-        String userid=userID;
-        String lat=this.lat;
-        String lng=this.lng;
-        String image=images;
+        String location = locationRe;
+        String methodof = methodOfReport;
+        String comment = commit.getText().toString();
+        String userid = userID;
+        String lat = this.lat;
+        String lng = this.lng;
+        String image = images;
 
-        String method="submitReport";
-        BackgroundTask backgroundTask=new BackgroundTask(getContext());
-        backgroundTask.execute(method,location,methodof,comment,userid,lat,lng,image);
+        String method = "submitReport";
+        BackgroundTask backgroundTask = new BackgroundTask(getContext());
+        backgroundTask.execute(method, location, methodof, comment, userid, lat, lng, image);
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        Log.d("mapppp",String.valueOf(resultCode));
-        if (requestCode==PLACE_PICKER_REQUEST){
-            if(resultCode== Activity.RESULT_OK){
-                Place place=PlacePicker.getPlace(data,getContext());
-                String address=String.format("Place : %s",place.getAddress());
-                Log.d("hi",address);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("mapppp", String.valueOf(resultCode));
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == Activity.RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, getContext());
+                String address = String.format("Place : %s", place.getAddress());
+                Log.d("hi", address);
             }
-        } else if(requestCode==GET_FROM_GALLERY && resultCode == Activity.RESULT_OK) {
+        } else if (requestCode == GET_FROM_GALLERY && resultCode == Activity.RESULT_OK) {
             Uri selectedImage = data.getData();
             Bitmap bitmap = null;
             try {
@@ -230,12 +249,13 @@ public class SubmitReport extends Fragment implements AdapterView.OnItemSelected
                 textView.setVisibility(View.GONE);
                 imageView.setVisibility(View.VISIBLE);
                 imageView.setImageBitmap(bitmap);
+                uri = selectedImage;
                 ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
                 bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
-                byte[] byteArray = byteArrayOutputStream .toByteArray();
+                byte[] byteArray = byteArrayOutputStream.toByteArray();
                 String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
-                Log.d("Base64",encoded);
-                images=encoded;
+                Log.d("Base64", encoded);
+                images = encoded;
             } catch (FileNotFoundException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
@@ -250,9 +270,9 @@ public class SubmitReport extends Fragment implements AdapterView.OnItemSelected
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mapView=(MapView) view.findViewById(R.id.map);
+        mapView = (MapView) view.findViewById(R.id.map);
 
-        if(mapView != null){
+        if (mapView != null) {
             mapView.onCreate(null);
             mapView.onResume();
             mapView.getMapAsync(this);
@@ -285,18 +305,18 @@ public class SubmitReport extends Fragment implements AdapterView.OnItemSelected
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String method=parent.getItemAtPosition(position).toString();
-        if(method.equals("Power Breakedown")){
-            methodOfReport="Power Breakedown";
+        String method = parent.getItemAtPosition(position).toString();
+        if (method.equals("Power Breakedown")) {
+            methodOfReport = "Power Breakedown";
 
-        }else if(method.equals("Maintainence")){
-            methodOfReport="Maintainence";
-        }else if(method.equals("Request new Connection")){
-            methodOfReport="Request new Connection";
-        }else if(method.equals("Request exist Connection")){
-            methodOfReport="Request exist Connection";
-        }else if(method.equals("Complains")){
-            methodOfReport="Complains";
+        } else if (method.equals("Maintainence")) {
+            methodOfReport = "Maintainence";
+        } else if (method.equals("Request new Connection")) {
+            methodOfReport = "Request new Connection";
+        } else if (method.equals("Request exist Connection")) {
+            methodOfReport = "Request exist Connection";
+        } else if (method.equals("Complains")) {
+            methodOfReport = "Complains";
         }
 
     }
@@ -307,13 +327,29 @@ public class SubmitReport extends Fragment implements AdapterView.OnItemSelected
     }
 
 
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         MapsInitializer.initialize(getContext());
 
-        this.googleMap=googleMap;
+        this.googleMap = googleMap;
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+//        CameraPosition srilanka= CameraPosition.builder().target(new LatLng(7.6697,80.6459)).zoom(8).bearing(0).tilt(45).build();
+//        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(srilanka));
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
     }
 
     /**
@@ -329,5 +365,79 @@ public class SubmitReport extends Fragment implements AdapterView.OnItemSelected
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    public String getPath(Uri uri) {
+        Log.d("Urieka",uri.toString());
+        Cursor cursor = getActivity().getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
+        Log.d("Uriekaaa",document_id);
+        cursor.close();
+
+        cursor = getContext().getContentResolver().query(
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, MediaStore.Images.Media._ID + " = ? ", new String[]{"/storage/emulated/0/Download/sparklogo.png"}, null);
+        cursor.moveToFirst();
+//        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+//        cursor.close();
+
+        return document_id;
+    }
+
+    public void uploadMultipart(Uri filePath, String URL) {
+        //getting name for the image
+        int time = (int) (System.currentTimeMillis());
+        Timestamp tsTemp = new Timestamp(time);
+        imageName =  tsTemp.toString();
+        //getting the actual path of the image
+        String path = getPath(filePath);
+
+        //Uploading code
+        try {
+            String uploadId = UUID.randomUUID().toString();
+
+            //Creating a multi part request
+            new MultipartUploadRequest(getContext(), uploadId, URL)
+                    .addFileToUpload(path, "image") //Adding file
+                    .addParameter("name", imageName) //Adding text parameter to the request
+//                    .setNotificationConfig(new UploadNotificationConfig())
+                    .setMaxRetries(2)
+                    .startUpload(); //Starting the upload
+
+        } catch (Exception exc) {
+            Toast.makeText(getContext(), exc.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            return true;
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            //If the user has denied the permission previously your code will come to this block
+            //Here you can explain why you need this permission
+            //Explain here why you need this permission
+        }
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+
+        return false;
+    }
+
+    private boolean requestLocationPermission() {
+        if((ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)&&(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED))
+            return true;
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            //If the user has denied the permission previously your code will come to this block
+            //Here you can explain why you need this permission
+            //Explain here why you need this permission
+        }
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION}, STORAGE_PERMISSION_CODE);
+
+        return false;
     }
 }
